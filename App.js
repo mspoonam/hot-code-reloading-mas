620ecd4abe19f45164929d51fe245d86a920402e
mspoonam <mspoonam.contact@gmail.com>
2018-05-07 16:14:50 +0530

    Added background color change to check Code Push works or not

--- App.js
+++ App.js
@@ -43,7 +43,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#00ff00',
     alignItems: 'center',
     justifyContent: 'center',
   },
